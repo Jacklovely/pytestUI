72278cfcf3cb4a6b3fafa72722a95f7ae4ce08d3
Jacklovely <48545645+Jacklovely@users.noreply.github.com>
2020-06-12 10:58:54 +0800

    Update read_yml.py

--- common/read_yml.py
+++ common/read_yml.py
@@ -10,7 +10,7 @@ class ReadYaml():
     def get_yaml_data(self):
         with open(self.filepath,'r',encoding='utf-8')as f:
             #调用load方法加载文件流
-            return yaml.load(f)
+            return yaml.load(f,Loader=yaml.FullLoader)
 
 if __name__ == '__main__':
     data = ReadYaml("login_page.yml").get_yaml_data()
